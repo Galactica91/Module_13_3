ed068aa91dd6a88d74be4047c0907a68daf573d4
Galactica91 <osheva91@yandex.com>
2024-12-04 14:10:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7775779452:AAEATs0Fe0bP-tyd7DfnOmL3nOPUGEWIEI4"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
